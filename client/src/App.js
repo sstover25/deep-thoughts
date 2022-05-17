96d84f087863b2058ff9b8dc312e0620ca2c3ebe
SSto <95592688+sstover25@users.noreply.github.com>
2022-05-17 13:50:24 -0500

    added navigation links and url params and built the Single Thought page

--- client/src/App.js
+++ client/src/App.js
@@ -36,8 +36,8 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/thought" element={<SingleThought />} />
+              <Route path="/profile/:username?" element={<Profile />} />
+              <Route path="/thought/:id" element={<SingleThought />} />
               <Route path="*" element={<NoMatch />} />
             </Routes>
           </div>
